32332caed6a37dcaf56359c0fdda51fdacfdf2a9
lunalovegood0219 <129015679+lunalovegood0219@users.noreply.github.com>
2023-08-26 16:23:30 +0330

    Update SearchEngine.js

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,7 +1,7 @@
 import React from "react";
 export default function SearchEngine (){
     return <div>
-        <form onClick={handleClick}>
+        <form>
             <input type="search" placeholder="Type a city..." autoComplete="off" />
             <input type="submit" className="btn btn-dark"/>
         </form>
