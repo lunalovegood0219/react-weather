21c7cf9f587d18cc7b06eac7179240afb47f7fd2
lunalovegood0219 <129015679+lunalovegood0219@users.noreply.github.com>
2023-08-27 15:51:49 +0330

    added spell check to search engine

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,18 +1,28 @@
 import React from "react";
-import "./SearchEngine.css"
-export default function SearchEngine (){
-    return (
-<div className="row">
-    <form class="mb-3 mt-3">
-    <div class="row">
-        <div class="col-md-9">
-    <input type="search" placeholder="Type a city..." className="form-control" autocomplete="off" />
+import "./SearchEngine.css";
+export default function SearchEngine() {
+  return (
+    <div className="row">
+      <form class="mb-3 mt-3">
+        <div class="row">
+          <div class="col-md-9">
+            <input
+              type="search"
+              placeholder="Type a city..."
+              className="form-control"
+              autocomplete="off"
+              spellCheck="true"
+            />
+          </div>
+          <div class="col-md-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-dark w-100 "
+            />
+          </div>
+        </div>
+      </form>
     </div>
-    <div class="col-md-3">
-    <input type="submit" value="Search" className="btn btn-dark w-100 " />
-    </div>
-</div>
-</form>
-</div>
-    );
+  );
 }
