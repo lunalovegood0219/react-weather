c2e88978d6bf5d9bd7cc3f16f9f15c0a5e1080fe
lunalovegood0219 <129015679+lunalovegood0219@users.noreply.github.com>
2023-08-29 16:08:11 +0330

    Added Api

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <div className="weather-container">
-        <SearchEngine />
+        <SearchEngine city="lisbon"/>
         <Weather />
         <Unit />
         <hr />
--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,20 +1,29 @@
 import React from "react";
 import "./SearchEngine.css";
-export default function SearchEngine() {
+import axios from "axios";
+export default function SearchEngine(props) {
+  function displayWeather(response) {
+    console.log(response.data);
+  }
+  function handleSearch() {
+    const apiKey = "51f128ddb960a0cbed5d6f3eea37ad01";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+  }
   return (
     <div className="row">
-      <form class="mb-3 mt-3">
-        <div class="row">
-          <div class="col-md-9">
+      <form className="mb-3 mt-3" onClick={handleSearch}>
+        <div className="row">
+          <div className="col-md-9">
             <input
               type="search"
               placeholder="Type a city..."
               className="form-control"
-              autocomplete="off"
-              spellCheck="true" 
+              autoComplete="off"
+              spellCheck="true"
             />
           </div>
-          <div class="col-3 ">
+          <div className="col-3 ">
             <input
               type="submit"
               value="Search"
