fa2997e0802099b9fcce07a8424ed78e3be65911
lunalovegood0219 <129015679+lunalovegood0219@users.noreply.github.com>
2023-08-29 15:45:29 +0330

    added unit links

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import Forcast from "./Forcast";
 import SearchEngine from "./SearchEngine";
 import Weather from "./Weather";
 import "bootstrap/dist/css/bootstrap.css"
+import Unit from "./Unit"
+
 function App() {
   return (
     <div className="App">
       <div className="weather-container">
         <SearchEngine />
         <Weather />
+        <Unit />
         <hr />
         <Forcast />
       </div>
       <div className="info">
-      <a href="https://github.com/lunalovegood0219/react-weather">
-        open-source code
-      </a>
-      <span>{" "}by Fatemeh Barzegar</span>
+        <a href="https://github.com/lunalovegood0219/react-weather">
+          open-source code
+        </a>
+        <span> by Fatemeh Barzegar</span>
       </div>
     </div>
   );
---
+++ src/Unit.js
@@ -0,0 +1,10 @@
+import React from "react";
+export default function Unit() {
+  return (
+    <div className="text-center">
+      <a href="/">°C</a>
+      <span>{" "}</span>
+      <a href="/">°F</a>
+    </div>
+  );
+}
