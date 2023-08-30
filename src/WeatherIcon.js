20a9f233ab796f6609121b92e352b615bf39be56
lunalovegood0219 <129015679+lunalovegood0219@users.noreply.github.com>
2023-08-30 21:08:15 +0330

    customized the icons

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -2,11 +2,30 @@ import React from "react";
 import ReactAnimatedWeather from "react-animated-weather";
 
 export default function WeatherIcon(props) {
+  const iconMaping = {
+    "01d": "CLEAR_DAY",
+    "01n": "CLEAR_NIGHT",
+    "02d": "PARTLY_CLOUDY_DAY",
+    "02n": "PARTLY_CLOUDY_NIGHT",
+    "03d": "PARTLY_CLOUDY_DAY",
+    "03n": "PARTLY_CLOUDY_NIGHT",
+    "04d": "CLOUDY",
+    "04n": "CLOUDY",
+    "09d": "SLEET",
+    "09n": "SLEET",
+    "10d": "RAIN",
+    "10n": "RAIN",
+    "11D": "RAIN",
+    "11N": "RAIN",
+    "13d": "SNOW",
+    "13n": "SNOW",
+    "50d": "FOG",
+  };
   return (
     <ReactAnimatedWeather
-      icon="CLEAR_DAY"
-      color="goldenrod"
-      size= {45}
+      icon={iconMaping[props.code]}
+      color="#212529"
+      size={45}
       animate={true}
     />
   );
